214ccfae42d751331f676319719ab00e358e55e9
Jurek Balwiński <jurek_b@wp.pl>
2020-08-26 21:04:59 +0200

    Initial. Working main window with test data. Using onMouseClick event

---
+++ src/com/jbalwinski/todolist/Controller.java
@@ -0,0 +1,64 @@
+package com.jbalwinski.todolist;
+
+import com.jbalwinski.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    @FXML
+    private Label deadlineLabel;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Buy flowers", "Buy flowers form moms birthday",
+                LocalDate.of(2020, Month.DECEMBER, 10));
+        TodoItem item2 = new TodoItem("Call plumber", "Fix tap in bathroom",
+                LocalDate.of(2020, Month.SEPTEMBER, 1));
+        TodoItem item3 = new TodoItem("John's performance at school", "Be 15 before 4 pm.",
+                LocalDate.of(2020, Month.NOVEMBER, 16));
+        TodoItem item4 = new TodoItem("Last day before Christmas", "Check presents list",
+                LocalDate.of(2020, Month.DECEMBER, 23));
+        TodoItem item5 = new TodoItem("Call Merry", "I promised her hangout just after NY",
+                LocalDate.of(2021, Month.JANUARY, 2));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+
+//        System.out.println("Selected item details:" + item.getDetails());
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadline().toString());
+//        itemDetailsTextArea.setText(sb.toString());
+    }
+}
