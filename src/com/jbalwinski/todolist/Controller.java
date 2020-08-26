9b7ab36f2e230bac98a66a872fa60c0e0dd28115
Jurek Balwiński <jurek_b@wp.pl>
2020-08-26 21:42:41 +0200

    Default item selected at start. Using change listener instead of onMouseClicked (however due date stoped do display)

--- src/com/jbalwinski/todolist/Controller.java
+++ src/com/jbalwinski/todolist/Controller.java
@@ -1,6 +1,8 @@
 package com.jbalwinski.todolist;
 
 import com.jbalwinski.todolist.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -44,8 +46,19 @@ public class Controller {
         todoItems.add(item4);
         todoItems.add(item5);
 
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observableValue, TodoItem oldValue, TodoItem newValue) {
+                if(newValue != null) {
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                        itemDetailsTextArea.setText(item.getDetails());
+                }
+            }
+        });
+
         todoListView.getItems().setAll(todoItems);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
         }
 
     @FXML
